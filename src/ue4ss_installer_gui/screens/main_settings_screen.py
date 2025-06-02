8cb9ea8373693aebd34acb20dcde8625d1048c0c
Mythical-Github <MythicalData@gmail.com>
2025-06-02 14:43:29 -0400

    Added more settings options

--- src/ue4ss_installer_gui/screens/main_settings_screen.py
+++ src/ue4ss_installer_gui/screens/main_settings_screen.py
@@ -1,11 +1,48 @@
 import os
 import sys
 import subprocess
+from typing import Any, Callable
 
 import dearpygui.dearpygui as dpg
 
-from ue4ss_installer_gui import settings
+from ue4ss_installer_gui import settings, auto_align, font, constants, file_io, grid
 import ue4ss_installer_gui.theme_management
+from ue4ss_installer_gui.screens import text_editor_screen
+
+
+def get_valid_language_options() -> list[str]:
+    specified_dir = os.path.normpath(f'{file_io.SCRIPT_DIR}/assets/localization')
+
+    if not os.path.isdir(specified_dir):
+        return []
+
+    language_options = [
+        os.path.splitext(file)[0]
+        for file in os.listdir(specified_dir)
+        if os.path.isfile(os.path.join(specified_dir, file))
+    ]
+
+    return language_options
+
+
+def change_font_button_selected(sender, app_data, user_data):
+    if dpg.does_item_exist("font_picker"):
+        dpg.delete_item("font_picker")
+
+    dpg.add_file_dialog(
+        directory_selector=False,
+        show=True,
+        callback=save_new_font,
+        cancel_callback=push_main_settings_screen,
+        tag="font_picker",
+        width=constants.WINDOW_WIDTH - 80,
+        height=constants.WINDOW_HEIGHT - 80,
+        modal=True,
+        file_count=999,
+        user_data=user_data,
+    )
+    dpg.add_file_extension(parent="font_picker", extension=".ttf")
+    dpg.add_file_extension(parent="font_picker", extension=".otf")
 
 
 def open_settings_file_in_default_text_editor(sender, app_data, user_data):
@@ -28,84 +65,8 @@ def open_settings_file_in_default_text_editor(sender, app_data, user_data):
         print(f"Failed to open settings file: {e}")
 
 
-def toggle_force_offline_mode_in_settings_file(sender, app_data, user_data):
-    loaded_settings = settings.get_settings()
-    gui_settings = loaded_settings.get("GUI", {})
-    gui_settings["use_force_offline_mode"] = app_data
-    loaded_settings["GUI"] = gui_settings
-    settings.save_settings(loaded_settings)
-
-
-def get_system_font_path():
-    import platform
-
-    system = platform.system()
-    if system == "Windows":
-        return os.path.normpath("C:/Windows/Fonts/arial.ttf")
-    elif system == "Linux":
-        possible_paths = [
-            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-            "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
-            "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
-        ]
-        for path in possible_paths:
-            if os.path.isfile(path):
-                return path
-    return None
-
-
-def toggle_use_custom_font_in_settings_file(sender, app_data, user_data):
-    # this needs to set the actual default font later on toggle off
-    # this needs to toggle disabling the group containing the font path and font change button
-    loaded_settings = settings.get_settings()
-    gui_settings = loaded_settings.get("GUI", {})
-    gui_settings["use_custom_font"] = app_data
-    loaded_settings["GUI"] = gui_settings
-    settings.save_settings(loaded_settings)
-
-    if app_data:
-        font_path = settings.get_settings().get("GUI", {}).get("custom_font_path", "")
-    else:
-        font_path = get_system_font_path()
-
-    if dpg.does_item_exist("font_reg"):
-        dpg.delete_item("font_reg")
-
-    if font_path and os.path.exists(font_path):
-        with dpg.font_registry(tag="font_reg"):
-            with dpg.font(font_path, 14) as custom_font:
-                dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
-                dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
-                dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
-        dpg.bind_font(custom_font)
-
-
-def add_centered_text(text, parent):
-    char_width = 7.25
-    available_width = 498
-
-    text_width = len(text) * char_width
-    center_x = int((available_width - text_width) / 2) - 2
-
-    with dpg.group(horizontal=True, parent=parent):
-        dpg.add_spacer(width=center_x)
-        dpg.add_text(text)
-
-
 def close_main_settings_menu():
     dpg.delete_item("main_settings_screen")
-
-
-def theme_selected(sender, app_data, user_data):
-    loaded_settings = settings.get_settings() or {}
-    gui_settings = loaded_settings.setdefault("GUI", {})
-    gui_settings["preferred_theme"] = app_data or "default"
-    settings.save_settings(loaded_settings)
-    dpg.bind_theme(
-        ue4ss_installer_gui.theme_management.get_theme_from_theme_name(  # type: ignore
-            app_data or "default"
-        )
-    )
 
 
 def push_main_settings_screen():
@@ -116,63 +77,95 @@ def push_main_settings_screen():
         tag="main_settings_screen",
         no_title_bar=True,
         no_open_over_existing_popup=False,
-        min_size=[524, 400],
-        max_size=[524, 999],
+        min_size=[524, 340],
+        max_size=[524, 9999],
         autosize=True,
         modal=True,
         no_move=True,
         no_resize=True,
     ):
-        add_centered_text("Settings", parent="main_settings_screen")
+        # auto_align.add_centered_text("Settings", parent="main_settings_screen")
+        auto_align.add_multi_line_centered_text("Settings", parent="main_settings_screen")
+
 
         dpg.add_separator()
         dpg.add_spacer(height=2)
 
+
+        with dpg.group(horizontal=True):
+            dpg.add_text("Global font scale")
+            dpg.add_drag_float(
+                default_value=settings.get_global_font_scale_from_settings(), 
+                speed=0.1, 
+                min_value=0.8, 
+                max_value=2, 
+                width=-1, 
+                callback=set_font_scale_and_save,
+                drop_callback=set_font_scale_and_save,
+                tag="global_font_scale_slider"
+            )
+
+
         with dpg.group(horizontal=True):
             dpg.add_checkbox(
-                callback=toggle_use_custom_font_in_settings_file,
-                default_value=settings.get_settings()
-                .get("GUI", {})
-                .get("use_custom_font", False),
+                callback=toggle_using_custom_font,
+                default_value=settings.get_use_custom_font_from_settings()
             )
             dpg.add_text("Use custom font")
-        with dpg.group(enabled=False, tag="font_holder"):
-            dpg.add_text(
-                default_value=f"Font path: {settings.get_settings().get('GUI', {}).get('custom_font_path', '')}"
-            )
-            dpg.add_button(label="Change font path", width=-1, height=28)
+        with dpg.group(enabled=settings.get_use_custom_font_from_settings(), tag="font_holder"):
+            dpg.add_text(default_value=f"Font path: {settings.get_custom_font_path_from_settings()}")
+            dpg.add_button(label="Change font path", width=-1, height=28, callback=change_font_button_selected)
+
 
         dpg.add_spacer(height=2)
         dpg.add_separator()
         dpg.add_spacer(height=2)
 
-        with dpg.group(horizontal=True):
-            dpg.add_checkbox()
-            dpg.add_text("Use language override")
-
-        with dpg.group(horizontal=True, enabled=False):
-            items = ["test", "test_two", "test_three"]
-            dpg.add_text(default_value="Language override")
-            dpg.add_combo(items=items, width=-1)
-
-        dpg.add_spacer(height=2)
-        dpg.add_separator()
-        dpg.add_spacer(height=2)
-
-        with dpg.group(horizontal=True):
-            dpg.add_checkbox(default_value=True)
-            dpg.add_text("Use automatic game detection scanning")
-
-        dpg.add_spacer(height=2)
-        dpg.add_separator()
-        dpg.add_spacer(height=2)
 
         with dpg.group(horizontal=True):
             dpg.add_checkbox(
-                callback=toggle_force_offline_mode_in_settings_file,
-                default_value=settings.get_settings()
-                .get("GUI", {})
-                .get("use_force_offline_mode", False),
+                callback=toggle_use_language_override, 
+                default_value=settings.get_use_language_override_from_settings()
+            )
+            dpg.add_text("Use language override (requires restart)")
+
+
+        with dpg.group(
+            horizontal=True, 
+            enabled=settings.get_use_language_override_from_settings(), 
+            tag='lang_override_group'
+        ):
+            dpg.add_text(default_value="Language override")
+            default_combo_value = settings.get_language_from_settings()
+            dpg.add_combo(
+                items=get_valid_language_options(), 
+                width=-1, 
+                callback=settings.language_combo_box_selection_changed, 
+                default_value=default_combo_value
+            )
+
+
+        dpg.add_spacer(height=2)
+        dpg.add_separator()
+        dpg.add_spacer(height=2)
+
+
+        with dpg.group(horizontal=True):
+            default = settings.get_use_automatic_game_scanning_in_settings()
+            dpg.add_checkbox(default_value=default, callback=settings.toggle_use_automatic_game_scanning_in_settings_file)
+            dpg.add_text("Use automatic game detection scanning")
+
+
+        with dpg.group(horizontal=False):
+            dpg.add_spacer(height=2)
+            dpg.add_separator()
+            dpg.add_spacer(height=2)
+
+
+        with dpg.group(horizontal=True):
+            dpg.add_checkbox(
+                callback=settings.toggle_force_offline_mode_in_settings_file,
+                default_value=settings.get_use_force_online_mode_in_settings()
             )
             dpg.add_text("Force offline mode (requires restart)")
 
@@ -184,9 +177,9 @@ def push_main_settings_screen():
             after_default_items = sorted(
                 theme_name
                 for theme_name in ue4ss_installer_gui.theme_management.theme_labels_to_themes
-                if theme_name != "default"
+                if theme_name != settings.get_default_theme_name()
             )
-            after_default_items.insert(0, "default")
+            after_default_items.insert(0, settings.get_default_theme_name())
             dpg.add_text(default_value="Theme")
             dpg.add_combo(
                 items=after_default_items,
@@ -199,19 +192,69 @@ def push_main_settings_screen():
         dpg.add_separator()
         dpg.add_spacer(height=2)
 
-        with dpg.group(horizontal=True, tag="test", horizontal_spacing=7):
-            dpg.add_button(label="Edit settings file", width=250, height=28)
-            dpg.add_button(
-                label="Open settings file",
-                width=250,
-                height=28,
-                callback=open_settings_file_in_default_text_editor,
-            )
+        screen_info = {
+            "finished_callback": push_main_settings_screen,
+            "file_path": settings.SETTINGS_FILE
+        }
 
-        dpg.add_spacer(height=2)
-        dpg.add_separator()
-        dpg.add_spacer(height=0)
+        example_variable: dict[str, dict[Callable[..., Any], dict[str, Any]]] = {
+            "button_1": {
+                dpg.add_button: {
+                    "label": "Edit settings file",
+                    "width": -1,
+                    "height": 28,
+                    "callback": text_editor_screen.push_text_editor_screen,
+                    "user_data": screen_info,
+                }
+            },
+            "button_2": {
+                dpg.add_button: {
+                    "label": "Open settings file",
+                    "width": -1,
+                    "height": 28,
+                    "callback": open_settings_file_in_default_text_editor
+                }
+            }
+        }
+        
+
+        grid.add_spaced_item_grid(example_variable)
 
         dpg.add_button(
             label="Close", height=28, width=-1, callback=close_main_settings_menu
         )
+
+
+def set_font_scale_and_save():
+    font_scale = dpg.get_value("global_font_scale_slider")
+    dpg.set_global_font_scale(font_scale)
+    settings.save_global_font_scale(font_scale=font_scale)
+
+
+def save_new_font(sender, app_data, user_data):
+    settings.save_custom_font_path_to_settings(app_data)
+    print(app_data["file_path_name"])
+    font.set_application_font()
+    push_main_settings_screen()
+    
+
+def toggle_using_custom_font(sender, app_data, user_data):
+    settings.toggle_use_custom_font_in_settings_file(app_data)
+    font.set_application_font()
+    dpg.configure_item('font_holder', enabled=app_data)
+
+
+def toggle_use_language_override(sender, app_data, user_data):
+    # when setting to off set it back to the current system local if valid otherwise english
+    settings.toggle_use_language_override_in_settings_file(app_data)
+    dpg.configure_item('lang_override_group', enabled=app_data)
+
+
+def theme_selected(sender, app_data, user_data):
+    settings.change_preferred_theme_in_settings(app_data)
+    dpg.bind_theme(
+        ue4ss_installer_gui.theme_management.get_theme_from_theme_name(  # type: ignore
+            app_data or "default"
+        )
+    )
+
