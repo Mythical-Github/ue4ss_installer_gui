39a2c43025b30ce0df1307b8a64844e32562d6bc
Mythical-Github <MythicalData@gmail.com>
2025-06-02 14:35:20 -0400

    Reworked default theme, and made the grey theme, the new default

--- src/ue4ss_installer_gui/themes/grey.py
+++ src/ue4ss_installer_gui/themes/grey.py
@@ -4,7 +4,6 @@ import dearpygui.dearpygui as dpg
 def create_theme():
     with dpg.theme() as theme:
         with dpg.theme_component(dpg.mvAll):
-            # dpg.add_theme_style(dpg.mvStyleVar_WindowRounding, 10)
             dpg.add_theme_style(dpg.mvStyleVar_ChildRounding, 8)
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 6)
             dpg.add_theme_style(dpg.mvStyleVar_GrabRounding, 6)
